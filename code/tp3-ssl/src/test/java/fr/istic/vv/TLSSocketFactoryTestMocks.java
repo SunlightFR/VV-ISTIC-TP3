274ce0e1716863309c5a5584d6c161352234e46b
Lucas Thébault <lucas.thebault22@gmail.com>
2025-01-05 22:50:20 +0100

    Test mock preparedSocket_NullProtocols

--- code/tp3-ssl/src/test/java/fr/istic/vv/TLSSocketFactoryTestMocks.java
+++ code/tp3-ssl/src/test/java/fr/istic/vv/TLSSocketFactoryTestMocks.java
@@ -8,8 +8,23 @@ import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 public class TLSSocketFactoryTestMocks {
 
+    @Test
+    public void preparedSocket_NullProtocols(){
+        TLSSocketFactory f = new TLSSocketFactory();
+        SSLSocket socket = mock(SSLSocket.class);
+        when(socket.getEnabledProtocols()).thenReturn(null);
+        when(socket.getSupportedProtocols()).thenReturn(null);
+        f.prepareSocket(mock(SSLSocket.class));
+        fail();
+    }
 
+    private String[] shuffle(String[] in) {
+        List<String> list = new ArrayList<String>(Arrays.asList(in));
+        Collections.shuffle(list);
+        return list.toArray(new String[0]);
+    }
 }
